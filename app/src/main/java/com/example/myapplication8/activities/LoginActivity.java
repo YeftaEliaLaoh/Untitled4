49b8e52c5fbf8781c713e05fc2f669bdb06d5c7a
Marry <yeftaelialaoh@rocketmail.com>
2020-08-14 23:51:35 +0700

    4

--- app/src/main/java/com/example/myapplication8/activities/LoginActivity.java
+++ app/src/main/java/com/example/myapplication8/activities/LoginActivity.java
@@ -1,13 +1,16 @@
 package com.example.myapplication8.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -21,9 +24,7 @@ import com.example.myapplication8.utilities.Config;
 public class LoginActivity extends AppCompatActivity
 {
 
-    private RelativeLayout demoModeLayout;
     private Switch demoModeSwitch;
-
     private EditText username;
     private EditText password;
     private Button loginBtn;
@@ -31,58 +32,41 @@ public class LoginActivity extends AppCompatActivity
 
 
     @Override
-    protected void onCreate( Bundle savedInstanceState )
+    protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initLayout();
         initEvent();
-
-        if( Config.IS_DEMO_VERSION )
-        {
-            setFieldToDemoVersion(false, true, R.color.light_grey);
-        }
+        setFieldToDemoVersion(Config.IS_DEMO_VERSION, R.color.light_grey);
     }
 
 
-    private void setFieldToDemoVersion( boolean isFieldEnable, boolean isDemoChecked, int textColor )
+    private void setFieldToDemoVersion(boolean isDemoChecked, int textColor)
     {
-
-        username.setEnabled(isFieldEnable);
-        password.setEnabled(isFieldEnable);
-
         username.setTextColor(getResources().getColor(textColor));
         password.setTextColor(getResources().getColor(textColor));
 
         String usernameTxt = Config.DEFAULT_STRING_VALUE;
-        String passwordTxt = usernameTxt;
+        String passwordTxt = Config.DEFAULT_STRING_VALUE; ;
+        Config.IS_DEMO_VERSION = false;
 
-        if( isDemoChecked )
+        if (isDemoChecked)
         {
-            usernameTxt = username.getText().toString();
-
-            if( usernameTxt.equals(Config.DEFAULT_STRING_VALUE) )
-            {
-                usernameTxt = Config.DEMO_USERNAME;
-                passwordTxt = Config.DEMO_PASSWORD;
-            }
+            usernameTxt = Config.DEMO_USERNAME;
+            passwordTxt = Config.DEMO_PASSWORD;
+            Config.IS_DEMO_VERSION = true;
         }
 
         username.setText(usernameTxt);
         password.setText(passwordTxt);
-
 
     }
 
     private void initLayout()
     {
 
-        LinearLayout loginContentLayout = (LinearLayout) findViewById(R.id.login_layout_content);
-        demoModeLayout = (RelativeLayout) findViewById(R.id.demo_mode_layout);
-        demoModeLayout.setVisibility(View.GONE);
         demoModeSwitch = (Switch) findViewById(R.id.demo_mode_switch);
-
-
         username = (EditText) findViewById(R.id.user_login);
         password = (EditText) findViewById(R.id.password_login);
         loginBtn = (Button) findViewById(R.id.login_btn);
@@ -96,31 +80,87 @@ public class LoginActivity extends AppCompatActivity
         loginBtn.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick( View v )
+            public void onClick(View v)
             {
+                showHiddenButton(loginBtn, View.GONE, R.anim.exit_to_right, R.color.blue_component_clicked);
+                showHiddenButton(cancelBtn, View.VISIBLE, R.anim.enter_from_left, R.color.colorRed);
+                if (Config.IS_DEMO_VERSION)
+                {
+                    successToLogin();
+                }
             }
         });
 
         cancelBtn.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick( View v )
+            public void onClick(View v)
             {
+                showHiddenButton(loginBtn, View.VISIBLE, R.anim.enter_from_right, R.color.blue_component);
+                showHiddenButton(cancelBtn, View.GONE, R.anim.exit_to_left, R.color.colorRedClicked);
             }
         });
 
         password.setOnEditorActionListener(new TextView.OnEditorActionListener()
         {
             @Override
-            public boolean onEditorAction( TextView v, int actionId, KeyEvent event )
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
             {
-                if( actionId == EditorInfo.IME_ACTION_DONE )
+                if (actionId == EditorInfo.IME_ACTION_DONE)
                 {
                     return true;
                 }
                 return false;
             }
         });
+
+        demoModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked)
+            {
+                changesComponentDemoMode(isChecked);
+            }
+        });
+    }
+
+    public void successToLogin()
+    {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void showHiddenButton(Button button, int visibility, int animation, int color)
+    {
+        Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), animation);
+
+        switch (visibility)
+        {
+            case View.VISIBLE:
+            case View.GONE:
+                button.setAnimation(anim);
+                break;
+            default:
+                break;
+        }
+
+        button.setBackgroundColor(getApplicationContext().getResources().getColor(color));
+        button.setVisibility(visibility);
+    }
+
+
+    private void changesComponentDemoMode(boolean isChecked)
+    {
+
+        if (isChecked)
+        {
+            setFieldToDemoVersion(true, R.color.light_grey);
+        }
+        else
+        {
+            setFieldToDemoVersion(false, R.color.white);
+        }
     }
 
 
@@ -140,7 +180,6 @@ public class LoginActivity extends AppCompatActivity
     public void onStop()
     {
         super.onStop();
-
     }
 
     @Override
@@ -150,11 +189,9 @@ public class LoginActivity extends AppCompatActivity
     }
 
     @Override
-    public void onRequestPermissionsResult( int requestCode, String[] permissions, int[] grantResults )
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
     {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        Log.d("Login Activity", "Request Permission Result");
 
     }
 
--- app/src/main/java/com/example/myapplication8/utilities/Config.java
+++ app/src/main/java/com/example/myapplication8/utilities/Config.java
@@ -4,7 +4,7 @@ public class Config
 {
     public static boolean IS_DEMO_VERSION = false;
     public static String DEFAULT_STRING_VALUE = "";
-    public static String DEMO_USERNAME = "superadmin-mt";
+    public static String DEMO_USERNAME = "superadmin";
     public static String DEMO_PASSWORD = "1234Qwe";
 
 }
