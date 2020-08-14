12616737bdf07d3761f8a82b3d567b26599c38e2
Yefta Elia <yafta.elia@1rstwap.com>
2020-08-14 15:16:26 +0700

    1

---
+++ app/src/main/java/com/example/myapplication8/activities/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication8.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication8.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
